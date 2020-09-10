e33d60735557f25fc6dd963dd391797b12ad3cf4
Loft579 <51039400+Loft579@users.noreply.github.com>
2020-09-10 20:41:43 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -16,7 +16,7 @@ async def on_ready():
 
 @bot.listen()
 async def on_voice_state_update(member, before, after):
-	global current_channel
+    global current_channel
     channel = after.channel
     if channel == None:
         channel = current_channel.guild.get_channel(current_channel.id)

fcf2524b8bda6f2e8359ad233c422919735b4d8c
Loft579 <51039400+Loft579@users.noreply.github.com>
2020-09-10 22:12:38 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -41,6 +41,8 @@ async def on_voice_state_update(member, before, after):
                         await asyncio.sleep(1)
                         await mmr.send(channel.members[0].name)
                         await mmr.send(invite.url)
+                    except:
+                        pass
                 await asyncio.sleep(20)
                 await channel.edit(name=channel.name.replace("📞↗","📞"))
             if channel.name.endswith("📞"):

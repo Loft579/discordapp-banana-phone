50a4a841a299bb1905f8ed9f130dcc3a59463762
Loft579 <51039400+Loft579@users.noreply.github.com>
2020-09-10 23:36:24 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -21,28 +21,29 @@ async def on_voice_state_update(member, before, after):
     if channel == None:
         channel = current_channel.guild.get_channel(current_channel.id)
     if channel != None:
-        if len(channel.members) == 1:
-            if channel.name.endswith("☎"):
-                current_channel = channel
-                await channel.edit(name=channel.name.replace("☎","📞↗"))
-                invite = await channel.create_invite(temporary=True)
-                for rol in member.roles:
-                    if "☎" in rol.name:
-                        connected = [] 
-                        for mmr in channel:
-                            connected.append(mmr.id)
-                        for mmr in rol.members:
-                            if not mmr.id in connected:
-                                try:
-                                    await mmr.send(rol.name)
-                                    await mmr.send(invite.url)
-                                    await asyncio.sleep(4)
-                                    await mmr.send(channel.members[0].name)
-                                    await mmr.send(invite.url)
-                            except:
-                                pass
-                await asyncio.sleep(20)
-                await channel.edit(name=channel.name.replace("📞↗","📞"))
+        if len(channel.members) <= 1:
+            if len(channel.members) == 1:
+                if channel.name.endswith("☎"):
+                    current_channel = channel
+                    await channel.edit(name=channel.name.replace("☎","📞↗"))
+                    invite = await channel.create_invite(temporary=True)
+                    for rol in member.roles:
+                        if "☎" in rol.name:
+                            connected = [] 
+                            for mmr in channel:
+                                connected.append(mmr.id)
+                            for mmr in rol.members:
+                                if not mmr.id in connected:
+                                    try:
+                                        await mmr.send(rol.name)
+                                        await mmr.send(invite.url)
+                                        await asyncio.sleep(1)
+                                        await mmr.send(channel.members[0].name)
+                                        await mmr.send(invite.url)
+                                except:
+                                    pass
+                    await asyncio.sleep(20)
+                    await channel.edit(name=channel.name.replace("📞↗","📞"))
             if channel.name.endswith("📞"):
                 await channel.edit(name=channel.name.replace("📞","☎"))
                 await channel.clone()
